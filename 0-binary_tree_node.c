5dfe627fb092f917cf476f4e2264f5ca25477591
besufikad <07yilmabese@gmail.com>
2023-08-29 09:39:10 +0300

    task 0&1

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,6 +2,19 @@
 #include"binary_trees.h"
 
 
+/**
+ * binary_tree_node - function creates a new node for a
+ * binary tree with the given value and parent.
+ *
+ * @parent: A pointer to the parent node of the new
+ * node being created. If the new node is the
+ * root node, the parent parameter should be set to NULL.
+ * @value: The value parameter is an integer value
+ * that will be assigned to the new node in the
+ * binary tree.
+ *
+ * Return: a pointer to a binary_tree_t structure.
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_ptr;
@@ -9,7 +22,7 @@ new_ptr = malloc(sizeof(binary_tree_t));
 if (new_ptr == NULL)
 {
 free(new_ptr);
-return (new_ptr);
+return (NULL);
 }
 new_ptr->n = value;
 new_ptr->right = NULL;
---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include"binary_trees.h"
+
+/**
+ * binary_tree_insert_left - function inserts a new node with the
+ * given value as the left child of the parent node in a
+ * binary tree.
+ *
+ * @parent: The parent parameter is a pointer to the
+ *  parent node of the binary tree.
+ * @value: The value parameter is an integer value
+ * that we want to insert into the binary tree.
+ *
+ * Return: a pointer to the newly inserted node in the binary tree.
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+binary_tree_t *new_ptr;
+binary_tree_t *holder;
+new_ptr = malloc(sizeof(binary_tree_t));
+if (new_ptr == NULL)
+return (NULL);
+
+new_ptr = binary_tree_node(parent, value);
+if (parent->left != NULL)
+{
+holder = parent->left;
+parent->left->parent = new_ptr;
+new_ptr->left = holder;
+}
+parent->left = new_ptr;
+return (new_ptr);
+}
