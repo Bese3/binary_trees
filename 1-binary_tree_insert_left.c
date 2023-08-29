ffa211f6d8e88ffdd9e28ac84c47036e49645034
besufikad <07yilmabese@gmail.com>
2023-08-29 10:09:46 +0300

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,18 +14,23 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-binary_tree_t *new_ptr;
-binary_tree_t *holder;
-new_ptr = binary_tree_node(parent, value);
-if (new_ptr == NULL || parent == NULL)
-return (NULL);
+	binary_tree_t *new_node;
 
-if (parent->left != NULL)
-{
-holder = parent->left;
-parent->left->parent = new_ptr;
-new_ptr->left = holder;
-}
-parent->left = new_ptr;
-return (new_ptr);
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	parent->left = new_node;
+	return (new_node);
 }
